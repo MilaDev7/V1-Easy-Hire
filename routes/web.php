2313ed830abe4fd43bae02653b48147e66221097
miladev7 <milionsahle96@gmail.com>
2026-03-30 16:50:51 +0300

    added rout home and search

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/search', function () {
+    return view('search');
+});
+
+Route::get('/client-dashboard', function () {
+    return view('client.dashboard');
+});
+
+
