c1ba9047aef2f9d306532267f58c23ee29e6eba0
miladev7 <milionsahle96@gmail.com>
2026-04-03 10:08:51 +0300

    added route log/register pro/clent setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Client\JobController;
 
 Route::get('/', function () {
     return view('home');
@@ -10,8 +11,32 @@ Route::get('/search', function () {
     return view('search');
 });
 
-Route::get('/client-dashboard', function () {
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+
+// Step 2: Role-Specific Setup Forms
+Route::get('/professional-setup', function () {
+    return view('auth.pro-setup');
+});
+Route::get('/client-setup', function () {
+    return view('auth.client-setup');
+});
+
+
+
+Route::get('/client/dashboard', function () {
     return view('client.dashboard');
 });
+
+
+
+
+
 
 
