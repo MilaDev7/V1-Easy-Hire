bdc2220f0f03507f1ce3f7a63acbbf357b3a3f70
miladev7 <milionsahle96@gmail.com>
2026-04-03 10:01:30 +0300

    create header/footer section

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,52 @@
+<!-- Footer -->
+<footer class="bg-dark text-white pt-5 pb-4 mt-5">
+    <div class="container">
+        <div class="row text-center text-md-start">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 fw-bold text-success">EasyHire</h5>
+                <p class="small text-secondary">
+                    Connecting clients with trusted professionals quickly and safely.
+                </p>
+                <p class="small text-secondary mt-3">
+                    <i class="fas fa-envelope me-2"></i> info@easyhire.com<br>
+                    <i class="fas fa-phone me-2"></i> +251 900000000
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold">Social Icons</h6>
+                <div class="d-flex justify-content-center justify-content-md-start gap-3">
+                    <a href="#" class="text-white fs-4"><i class="fab fa-facebook"></i></a>
+                    <a href="#" class="text-white fs-4"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white fs-4"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold">About</h6>
+                <ul class="list-unstyled small">
+                    <li class="mb-2"><a href="#" class="text-secondary text-decoration-none hover-white">About us</a></li>
+                    <li class="mb-2"><a href="#" class="text-secondary text-decoration-none hover-white">Professionals</a></li>
+                    <li class="mb-2"><a href="#" class="text-secondary text-decoration-none hover-white">About</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold">Team</h6>
+                <ul class="list-unstyled small text-secondary">
+                    <li class="mb-2">MILA</li>
+                    <li class="mb-2">Sentayhu</li>
+                    <li class="mb-2">Filimon</li>
+                    <li class="mb-2">Samual</li>
+                </ul>
+            </div>
+        </div>
+
+        <hr class="mb-4 border-secondary opacity-25">
+<div class="row">
+                <div class="col-12 text-center">
+                    <p class="small text-secondary mb-0">Baba 2026</p>
+                </div>
+            </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,34 @@
+ <!-- navbar -->
+
+
+ <nav class="navbar navbar-expand-lg bg-white shadow-sm sticky-top">
+     <div class="container">
+
+         <!-- Logo -->
+         <a class="navbar-brand fw-bold text-success" href="/">
+             EasyHire
+         </a>
+
+         <!-- Toggle (mobile) -->
+                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+
+         <!-- Links -->
+         <div class="collapse navbar-collapse" id="nav">
+             <ul class="navbar-nav mx-auto">
+                 <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                 <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                 <li   class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                 <li       class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+             </ul>     
+             <!-- Auth Buttons -->
+             <div class="d-flex gap-2">
+                 <a href="/register" class="btn btn-success">Register</a>
+                 <a href="/login" class="btn btn-outline-success">Login</a>
+
+             </div>
+         </div>
+
+     </div>
+ </nav>
